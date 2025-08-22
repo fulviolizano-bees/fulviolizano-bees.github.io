9013a3af0b13b412d5dfa72568969a90ff575046
fulviolizano-bees <fulviolizano@gmail.com>
2025-08-22 10:18:49 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind(), sitemap()],
-    site: template.website_url,
+    site: fulviolizano-bees,
     base: template.base,
 });
